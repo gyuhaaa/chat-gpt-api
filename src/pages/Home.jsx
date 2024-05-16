aa294e6721bc6146ca6c692784d0119744ed8857
gyuhaaa <vkvkvk2892@gmail.com>
2024-05-16 09:07:04 +0900

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [content, setContent] = useState("초기값");
+
+  const onSubmitChat = (e) => {
+    e.preventDefault();
+
+    // text 질문을 입력 받아서 서버로 ㄱㄱ
+  };
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitChat}>
+        <input
+          type="text"
+          value={content}
+          onChange={() => setContent(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
